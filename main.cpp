9c5eccd269371ea9e7892e3c1f5e05eca554fec2
Denis N <nov_den@mail.ru>
2020-11-16 20:58:48 +0300

    подправил внешний вид кода

--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-#include "conio.h"
-#include "stdlib.h"
-#include "string.h"
-
+//Просто расчеты
 
 #include <stdio.h>
 int main (void)
@@ -9,27 +6,37 @@ int main (void)
     float a = 5, b;
     int c = 5, d;
 
-    printf("float b = ");
+    printf("Vvedite b = ");
     scanf("%f", &b);
-    printf("int d = ");
+    printf( "\n" );
+    printf("Vvedite d = ");
     scanf("%d", &d);
     printf("a = %f, b = %f, c = %d, d = %d\n", a, b, c, d);
+    printf( "\n" );
 
     a += b - 2;
+    printf("a += b - 2\n");
     printf("a = %f, b = %f, c = %d, d = %d\n", a, b, c, d);
+    printf( "\n" );
 
     d += ++c - a;
+    printf("d += ++c - a\n");
 
     printf("a = %f, b = %f, c = %d, d = %d\n", a, b, c, d);
-
+    printf( "\n" );
 
     a *= c--;
+    printf("a *= c-- \n");
+
     printf("a = %f, b = %f, c = %d, d = %d\n", a, b, c, d);
+    printf( "\n" );
+
     a /= 10;
     c /= 2;
     d *= c + --b + a;
+    printf("d *= c + --b + a\n");
     printf("a = %f, b = %f, c = %d, d = %d\n", a, b, c, d);
+    printf( "\n" );
 
-    getch();
     return 0;
 }
